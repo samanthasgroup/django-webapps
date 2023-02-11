393af691eb7e92e0ca1713413dc63e0224172ac6
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-02-11 14:16:27 +0300

    add tables for enumerating possible statuses

--- api/models.py
+++ api/models.py
@@ -60,8 +60,38 @@ class DayAndTimeSlot(models.Model):
 
 
 # STATUSES
-class Status(models.Model):
+class StatusName(models.Model):
+    """Abstract model for classes enumerating possible statuses."""
+
     name = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+
+# We could create one table listing all possible status names, but that might look confusing
+# for admin users later on.  It seems more convenient for them to have separate tables.
+class CoordinatorStatusName(StatusName):
+    """Model for enumeration of possible statuses of a coordinator."""
+
+
+class GroupStatusName(StatusName):
+    """Model for enumeration of possible statuses of a group."""
+
+
+class StudentStatusName(StatusName):
+    """Model for enumeration of possible statuses of a student."""
+
+
+class TeacherStatusName(StatusName):
+    """Model for enumeration of possible statuses of a teacher."""
+
+
+class Status(models.Model):
+    """Abstract model for an actual status of a group or person. Combines a status name
+    (selected from allowed status names) with a time that indicates when this status was assigned.
+    """
+
     # this can be tracked via LogItems but an additional column can be a very convenient shortcut:
     in_place_since = models.DateTimeField(auto_now_add=True)
 
@@ -72,17 +102,25 @@ class Status(models.Model):
 class CoordinatorStatus(Status):
     """Model for statuses of coordinators."""
 
+    name = models.ForeignKey(CoordinatorStatusName, on_delete=models.CASCADE)
+
 
 class GroupStatus(Status):
     """Model for statuses of groups."""
+
+    name = models.ForeignKey(GroupStatusName, on_delete=models.CASCADE)
 
 
 class StudentStatus(Status):
     """Model for statuses of students."""
 
+    name = models.ForeignKey(StudentStatusName, on_delete=models.CASCADE)
+
 
 class TeacherStatus(Status):
     """Model for statuses of teachers."""
+
+    name = models.ForeignKey(TeacherStatusName, on_delete=models.CASCADE)
 
 
 # BLOCKS WITH ADDITIONAL INFORMATION ON A PERSON WITH A SPECIFIC ROLE
