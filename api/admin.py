0ca94741b37bb4a40b74312a2e18f35db0a3ee14
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-02-11 20:38:39 +0300

    register models with admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,30 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+import api.models as models
+
+for model in (
+    models.Coordinator,
+    models.CoordinatorLogItemName,
+    models.CoordinatorStatusName,
+    models.DayOfWeek,
+    models.EnrollmentTest,
+    models.EnrollmentTestQuestion,
+    models.EnrollmentTestQuestionOption,
+    models.Group,
+    models.GroupLogItemName,
+    models.GroupStatusName,
+    models.InformationSource,
+    models.LanguageLevel,
+    models.NativeLanguage,
+    models.PersonalInfo,
+    models.Student,
+    models.StudentLogItemName,
+    models.StudentStatusName,
+    models.Teacher,
+    models.TeacherCategory,
+    models.TeacherLogItemName,
+    models.TeacherStatusName,
+    models.TeachingLanguage,
+    models.TimeSlot,
+):
+    admin.site.register(model)
