6a10c32bd772a4b1237cc85e221fff652eb10464
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-04 17:08:37 +0300

    add `name_internal` to `InternalModelWithName`, `name`->`name_for_user`

--- api/models/auxil.py
+++ api/models/auxil.py
@@ -1,20 +1,27 @@
 from django.db import models
 
 
-class MultilingualModel(models.Model):
-    """Abstract model for end-user facing entities that need names to be stored in 3 languages."""
-
+class ModelWithName(models.Model):
     name_internal = models.CharField(
         max_length=50,
         unique=True,
         help_text=(
             "Internal name to use in code. This will allow to change user-facing names easily "
-            "without breaking the code."
+            "without breaking the code. Internal name must not change after adding it."
         ),
+        verbose_name="internal name",
     )
-    name_en = models.CharField(max_length=255, unique=True)
-    name_ru = models.CharField(max_length=255, unique=True)
-    name_ua = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class MultilingualModel(ModelWithName):
+    """Abstract model for end-user facing entities that need names to be stored in 3 languages."""
+
+    name_en = models.CharField(max_length=255, unique=True, verbose_name="name in English")
+    name_ru = models.CharField(max_length=255, unique=True, verbose_name="name in Russian")
+    name_ua = models.CharField(max_length=255, unique=True, verbose_name="name in Ukrainian")
 
     class Meta:
         abstract = True
@@ -27,16 +34,19 @@ class MultilingualModel(models.Model):
         return self.name_en
 
 
-class InternalModelWithName(models.Model):
+class InternalModelWithName(ModelWithName):
     """Abstract model for internal entities that have a name attribute but do not need to support
     internationalization.
     """
 
-    name = models.CharField(max_length=100)
+    name_for_user = models.CharField(max_length=100, verbose_name="Readable name for coordinator")
 
     class Meta:
         abstract = True
-        constraints = [models.UniqueConstraint(fields=["name"], name="f%(class)s_name")]
+        # I have to repeat this: if I put it in the parent class, it will not be inherited.
+        constraints = [
+            models.UniqueConstraint(fields=["name_internal"], name="f%(class)s_name_internal")
+        ]
 
     def __str__(self):
-        return self.name
+        return self.name_for_user
