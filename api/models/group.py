5a16b453899b3520a4ab50d89d0d2596c60e9c50
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-04 18:27:21 +0300

    add `GroupCommon` and `SpeakingClub`

--- api/models/group.py
+++ api/models/group.py
@@ -1,19 +1,38 @@
 from django.db import models
 
 from api.models.days_time_slots import DayAndTimeSlot
-from api.models.languages_levels import CommunicationLanguageMode, TeachingLanguageAndLevel
+from api.models.languages_levels import (
+    CommunicationLanguageMode,
+    TeachingLanguage,
+    TeachingLanguageAndLevel,
+)
 from api.models.people import Coordinator, Student, Teacher
 from api.models.statuses import GroupStatus
 
 
+class GroupCommon(models.Model):
+    """Abstract model for attributes shared by regular groups and speaking clubs."""
+
+    coordinators = models.ManyToManyField(Coordinator)
+    students = models.ManyToManyField(Student)
+    teachers = models.ManyToManyField(Teacher)
+    # group chat created manually by the coordinator/teacher
+    telegram_chat_url = models.URLField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
 # TODO think about group clusters for auto-constructed groups
-class Group(models.Model):
+class Group(GroupCommon):
+    """Model for a regular language group."""
+
     availability_slot = models.ManyToManyField(DayAndTimeSlot)
     communication_language_mode = models.ForeignKey(
         CommunicationLanguageMode, on_delete=models.PROTECT
     )
     is_for_staff_only = models.BooleanField(default=False)
-    language_and_level = models.ForeignKey(TeachingLanguageAndLevel, on_delete=models.CASCADE)
+    language_and_level = models.ForeignKey(TeachingLanguageAndLevel, on_delete=models.PROTECT)
     lesson_duration = models.PositiveSmallIntegerField()
     status = models.ForeignKey(GroupStatus, on_delete=models.PROTECT)
     status_since = models.DateTimeField(auto_now=True)
@@ -21,12 +40,6 @@ class Group(models.Model):
     # this field could be useful for overview, but can be filled automatically when
     # a corresponding log event is created:
     end_date = models.DateField(blank=True, null=True)
-    # group chat created manually by the coordinator/teacher
-    telegram_chat_url = models.URLField(blank=True, null=True)
-
-    coordinators = models.ManyToManyField(Coordinator)
-    students = models.ManyToManyField(Student)
-    teachers = models.ManyToManyField(Teacher)
 
     # some research showed that it's better to store the schedule not in a single text field
     # with some pre-defined syntax, but in 7 columns, one per day of the week
@@ -47,6 +60,22 @@ class Group(models.Model):
         coordinator_names = ",".join(c.full_name for c in self.coordinators.all())
         teacher_names = ",".join(t.full_name for t in self.teachers.all())
         return (
-            f"Group {self.pk} (coordinators: {coordinator_names}, teachers: {teacher_names}, "
-            f"{len(self.students.all())} students."
+            f"Group {self.pk}, {self.language_and_level} (coordinators: {coordinator_names}, "
+            f"teachers: {teacher_names}, {len(self.students.all())} students."
+        )
+
+
+class SpeakingClub(GroupCommon):
+    """Model for a speaking club (a group without any changing status or fixed schedule)."""
+
+    is_for_children = models.BooleanField(
+        verbose_name="Is this a speaking club for children?", default=False
+    )
+    # a speaking club has no fixed level, so putting language only
+    language = models.ForeignKey(TeachingLanguage, on_delete=models.PROTECT)
+
+    def __str__(self):
+        category = "children" if self.is_for_children else "adults"
+        return (
+            f"{self.language} speaking club for {category}, {len(self.students.all())} students."
         )
