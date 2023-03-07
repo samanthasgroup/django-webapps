bbf4023360107626d6cc5ba7a06b1e58413c6445
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-07 14:18:29 +0300

    refactor language level choices to use `models.TextChoices`

--- api/models/languages_levels.py
+++ api/models/languages_levels.py
@@ -24,24 +24,17 @@ class TeachingLanguage(ModelWithMultilingualName):
 
 
 class TeachingLanguageAndLevel(models.Model):
-    A0 = "A0"
-    A1 = "A1"
-    A2 = "A2"
-    B1 = "B1"
-    B2 = "B2"
-    C1 = "C1"
-    # This school is definitely not for C2 students, so no C2
-    LEVEL_CHOICES = [
-        (A0, "A0 (Starter)"),
-        (A1, "A1 (Beginner)"),
-        (A2, "A2 (Pre-Intermediate)"),
-        (B1, "B1 (Intermediate)"),
-        (B2, "B2 (Upper-Intermediate)"),
-        (C1, "C1 (Advanced)"),
-    ]
+    class Level(models.TextChoices):
+        A0 = "A0", "A0 (Starter)"
+        A1 = "A1", "A1 (Beginner)"
+        A2 = "A2", "A2 (Pre-Intermediate)"
+        B1 = "B1", "B1 (Intermediate)"
+        B2 = "B2", "B2 (Upper-Intermediate)"
+        C1 = "C1", "C1 (Advanced)"
+        # This school is definitely not for C2 students, so no C2
 
     language = models.ForeignKey(TeachingLanguage, on_delete=models.CASCADE)
-    level = models.CharField(max_length=2, choices=LEVEL_CHOICES)
+    level = models.CharField(max_length=2, choices=Level.choices)
 
     class Meta:
         verbose_name_plural = "Teaching languages with levels"
