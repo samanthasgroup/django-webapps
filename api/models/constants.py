1d150ab4c52125216f7525588c3573db1610e754
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-13 00:43:57 +0300

    constants: change age ranges to match upper boundaries of strings

--- api/models/constants.py
+++ api/models/constants.py
@@ -3,67 +3,48 @@ DEFAULT_CHOICE_CHAR_FIELD_MAX_LENGTH = 15
 
 # IMPORTANT: the boundaries of larger ranges must match the boundaries of the smaller ones
 
-# Match string ranges that are presented to the user to actual ranges.
-# A dict comprehension would be more concise but less readable.  Dict comprehension would also be
-# less error-prone, but the dict isn't supposed to be changed frequently (or at all) after launch.
+# Match string ranges that are presented to the user to actual ranges: {"5-7": (5, 7), ...}
 STUDENT_AGE_RANGES = {
-    "5-6": (5, 7),
-    "7-8": (7, 9),
-    "9-10": (9, 11),
-    "11-12": (11, 13),
-    "13-14": (13, 15),
-    "15-17": (15, 18),
-    "18-20": (18, 21),
-    "21-25": (21, 26),
-    "26-30": (26, 31),
-    "31-35": (31, 36),
-    "36-40": (36, 41),
-    "41-45": (41, 46),
-    "46-50": (46, 51),
-    "51-55": (51, 56),
-    "56-60": (56, 61),
-    "61-65": (61, 66),
-    "66-70": (66, 71),
-    "71-75": (71, 76),
-    "76-80": (76, 81),
-    "81-65": (81, 66),
-    "86-90": (86, 91),
-    "91-95": (91, 96),
+    f"{left}-{right}": (left, right)
+    for left, right in (
+        (5, 6),
+        (7, 8),
+        (9, 10),
+        (11, 12),
+        (13, 14),
+        (15, 17),
+        (18, 20),
+        (21, 25),
+        (26, 30),
+        (31, 35),
+        (36, 40),
+        (41, 45),
+        (46, 50),
+        (51, 55),
+        (56, 60),
+        (61, 65),
+        (66, 70),
+        (71, 75),
+        (76, 80),
+        (81, 65),
+        (86, 90),
+        (91, 95),
+    )
 }
 
 # Match string ranges presented to the teacher (for them to choose desired age groups of students)
 # to actual ranges
 STUDENT_AGE_RANGES_FOR_TEACHER = {
-    "5-8": (5, 9),
-    "9-12": (9, 13),
-    "13-17": (13, 18),
-    "18-": (18, 96),
+    f"{left}-{right}": (left, right)
+    for left, right in (
+        (5, 8),
+        (9, 12),
+        (13, 17),
+        (18, 95),
+    )
 }
 
 # age ranges for matching algorithm
 # TODO for now leaving these ranges the same as the ranges that students get when registering,
 #  but this is likely to change.  The children's ranges will most likely remain the same, though.
-STUDENT_AGE_RANGES_FOR_MATCHING = {
-    "5-6": (5, 7),
-    "7-8": (7, 9),
-    "9-10": (9, 11),
-    "11-12": (11, 13),
-    "13-14": (13, 15),
-    "15-17": (15, 18),
-    "18-20": (18, 21),
-    "21-25": (21, 26),
-    "26-30": (26, 31),
-    "31-35": (31, 36),
-    "36-40": (36, 41),
-    "41-45": (41, 46),
-    "46-50": (46, 51),
-    "51-55": (51, 56),
-    "56-60": (56, 61),
-    "61-65": (61, 66),
-    "66-70": (66, 71),
-    "71-75": (71, 76),
-    "76-80": (76, 81),
-    "81-65": (81, 66),
-    "86-90": (86, 91),
-    "91-95": (91, 96),
-}
+STUDENT_AGE_RANGES_FOR_MATCHING = STUDENT_AGE_RANGES.copy()
