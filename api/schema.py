79838e859237c898bd0844726650c814236638ff
Roxe322 <kolomatskiy@bk.ru>
2023-03-16 17:23:46 +0400

    Review fixes (comment and typing)

--- api/schema.py
+++ api/schema.py
@@ -2,7 +2,12 @@ from drf_spectacular.openapi import AutoSchema
 
 
 class TaggedByViewSetNameSchema(AutoSchema):
-    def get_tags(self) -> [str]:
+    """
+    Default schema uses same tag (from url) for all endpoints.
+    To make it more clear, we use viewset name as additional tag.
+    """
+
+    def get_tags(self) -> list[str]:
         tags = super().get_tags()
         view_set_tag_name = self.view.__class__.__name__.replace("ViewSet", "")
         return tags + [view_set_tag_name]
