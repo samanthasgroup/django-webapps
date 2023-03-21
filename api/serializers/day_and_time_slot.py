7b829e1755e5595e4bbefa313dfc136b726f8d3e
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-21 21:06:47 +0300

    Revert "Revert "fix several mypy issues (some generic type errors)""
    This reverts commit b8edde72bab2dc98f01b684dffa826cdefea9345.

--- api/serializers/day_and_time_slot.py
+++ api/serializers/day_and_time_slot.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from api.models import DayAndTimeSlot, TimeSlot
 
 
-class TimeSlotSerializer(serializers.ModelSerializer):
+class TimeSlotSerializer(serializers.ModelSerializer[TimeSlot]):
     class Meta:
         model = TimeSlot
         fields = "__all__"
 
 
-class DayAndTimeSlotSerializer(serializers.ModelSerializer):
+class DayAndTimeSlotSerializer(serializers.ModelSerializer[DayAndTimeSlot]):
     time_slot = TimeSlotSerializer(read_only=True)
 
     class Meta:
