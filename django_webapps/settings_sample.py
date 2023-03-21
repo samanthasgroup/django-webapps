5c4b7e487c8fad4ee4e20329ff87269cd2f70085
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-03-21 21:28:21 +0300

    settings_sample.py: apply `django_stubs_ext.monkeypatch()`
    this finally solves both the mypy and the django complaints
    some mypy issues still remain

--- django_webapps/settings_sample.py
+++ django_webapps/settings_sample.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
 
+import django_stubs_ext
+from rest_framework import viewsets
+
+# This is needed to be able to use annotations like "viewsets.ReadOnlyModelViewSet[Teacher]"
+# for mypy to stop ****ing our brains.
+django_stubs_ext.monkeypatch(extra_classes=[viewsets.ModelViewSet, viewsets.ReadOnlyModelViewSet])
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
