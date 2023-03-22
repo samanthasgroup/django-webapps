b5a93eae1c484a0f0c5c7483ed81a6fdb386efb9
Roxe322 <kolomatskiy@bk.ru>
2023-03-23 03:55:49 +0400

    added day_and_time_slots tests

---
+++ tests/tests_api/test_day_and_time_slots.py
@@ -0,0 +1,37 @@
+from rest_framework import status
+
+from api.models import DayAndTimeSlot
+
+
+def test_day_and_time_slot_list(admin_client):
+    queryset = DayAndTimeSlot.objects.all()
+    response = admin_client.get("/api/day_and_time_slots/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [
+        {
+            "id": day_and_time_slot.id,
+            "time_slot": {
+                "id": day_and_time_slot.time_slot.id,
+                "from_utc_hour": str(day_and_time_slot.time_slot.from_utc_hour),
+                "to_utc_hour": str(day_and_time_slot.time_slot.to_utc_hour),
+            },
+            "day_of_week_index": day_and_time_slot.day_of_week_index,
+        }
+        for day_and_time_slot in queryset
+    ]
+
+
+def test_day_and_time_slots_retrieve(admin_client):
+    day_and_time_slot = DayAndTimeSlot.objects.first()
+    response = admin_client.get(f"/api/day_and_time_slots/{day_and_time_slot.id}/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": day_and_time_slot.id,
+        "time_slot": {
+            "id": day_and_time_slot.time_slot.id,
+            "from_utc_hour": str(day_and_time_slot.time_slot.from_utc_hour),
+            "to_utc_hour": str(day_and_time_slot.time_slot.to_utc_hour),
+        },
+        "day_of_week_index": day_and_time_slot.day_of_week_index,
+    }
