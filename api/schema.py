9799ba2fbf1ee44735f6af670db9a2f8350b083c
Artem Kolomatskiy <38218668+Roxe322@users.noreply.github.com>
2023-03-27 05:10:05 +0400

    Update api/schema.py
    Co-authored-by: Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>

--- api/schema.py
+++ api/schema.py
@@ -13,7 +13,7 @@ class CustomSchema(AutoSchema):
     Differences from default schema:
     - Default schema uses same tag - first non-parameter path part as tag for all endpoints.
       (e.g. /api/users/ -> tag: api and also /api/other/ -> tag: api).
-      To make it more clear, we use viewset name as additional tag.
+      To make it more clear, we override the get_tags() method to include viewset name as additional tag.
     - We add 400 response for all endpoints with unsafe method.
       (Also some other responses can be added here in the future, such as 403 etc.).
     """
