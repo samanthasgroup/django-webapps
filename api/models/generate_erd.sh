6790f2b507684a455133af4b3038f36bf84f55f8
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-04-03 18:58:45 +0300

    add rendering of core models for them to be more readable for owners

--- api/models/generate_erd.sh
+++ api/models/generate_erd.sh
@@ -3,3 +3,8 @@
 python ../../manage.py graph_models --output erd.dot --theme django2018 --layout fdp --rankdir TB --arrow crow --verbosity 2 api
 dot -Tpng erd.dot -o erd.png
 rm erd.dot
+
+# now the small graph (with verbose names) for the "core" models: personal info and roles
+python ../../manage.py graph_models --output erd_core.dot --verbose-names -I PersonalInfo,Coordinator,Student,Teacher --theme django2018 --layout fdp --rankdir TB --arrow crow --verbosity 2 api
+dot -Tpng erd_core.dot -o erd_core.png
+rm erd_core.dot
