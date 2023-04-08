77b7e79d056daa5a4bb6bfa50d7a8a07cf526b22
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-04-08 12:54:00 +0300

    add `TeacherUnder18` to core ERD, generate ERDs

--- api/models/generate_erd.sh
+++ api/models/generate_erd.sh
@@ -5,6 +5,6 @@ dot -Tpng erd.dot -o erd.png
 rm erd.dot
 
 # now the small graph (with verbose names) for the "core" models: personal info and roles
-python ../../manage.py graph_models --output erd_core.dot --verbose-names -I PersonalInfo,Coordinator,Student,Teacher --theme django2018 --layout fdp --rankdir TB --arrow crow --verbosity 2 api
+python ../../manage.py graph_models --output erd_core.dot --verbose-names -I PersonalInfo,Coordinator,Student,Teacher,TeacherUnder18 --theme django2018 --layout fdp --rankdir TB --arrow crow --verbosity 2 api
 dot -Tpng erd_core.dot -o erd_core.png
 rm erd_core.dot
