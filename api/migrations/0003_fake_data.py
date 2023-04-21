9f7cfd9bc187af9b8297b644b89e5095a5bac57a
Roxe322 <kolomatskiy@bk.ru>
2023-04-21 07:15:21 +0400

    Added Groups and SpeakingClubs

--- api/migrations/0003_fake_data.py
+++ api/migrations/0003_fake_data.py
@@ -5,23 +5,37 @@ from django.db.backends.sqlite3.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 from django.db.models import Q
 from faker import Faker
-from model_bakery.recipe import Recipe, foreign_key
+from model_bakery.recipe import Recipe, foreign_key, related
 
-from api.models import AgeRange, DayAndTimeSlot, NonTeachingHelp, LanguageAndLevel
+from api.models import (
+    AgeRange,
+    DayAndTimeSlot,
+    NonTeachingHelp,
+    LanguageAndLevel,
+    Language,
+)
 from api.models.choices.age_range_type import AgeRangeType
 from api.models.choices.communication_language_mode import CommunicationLanguageMode
 from api.models.choices.registration_telegram_bot_language import (
     RegistrationTelegramBotLanguage,
 )
-from api.models.choices.statuses import CoordinatorStatus, StudentStatus, TeacherStatus, TeacherUnder18Status
+from api.models.choices.statuses import (
+    CoordinatorStatus,
+    StudentStatus,
+    TeacherStatus,
+    TeacherUnder18Status,
+    GroupStatus,
+)
 from api.models.data_migration_maker import DataMigrationMaker
 
 APP_NAME = "api"
 
-AMOUNT_OF_COORDINATORS = 10
-AMOUNT_OF_STUDENTS = 100
-AMOUNT_OF_TEACHERS = 25
-AMOUNT_OF_TEACHERS_UNDER_18 = 5
+AMOUNT_OF_COORDINATORS_WITHOUT_GROUP = 10
+AMOUNT_OF_GROUPS = 30
+AMOUNT_OF_STUDENTS_WITHOUT_GROUP = 30
+AMOUNT_OF_TEACHERS_WITHOUT_GROUP = 5
+AMOUNT_OF_TEACHERS_UNDER_18_WITHOUT_SPEAKING_CLUB = 5
+AMOUNT_OF_SPEAKING_CLUBS = 7
 
 
 class FakeDataPopulator(DataMigrationMaker):
@@ -33,6 +47,8 @@ class FakeDataPopulator(DataMigrationMaker):
         self.student_recipe = self._make_student_recipe()
         self.teacher_recipe = self._make_teacher_recipe()
         self.teacher_under_18_recipe = self._make_teacher_under_18_recipe()
+        self.group_recipe = self._make_group_recipe()
+        self.speaking_club_recipe = self._make_speaking_club_recipe()
 
     def _get_random_amount_of_objects(
         self,
@@ -50,7 +66,27 @@ class FakeDataPopulator(DataMigrationMaker):
             length=self.faker.pyint(min_value=min_length, max_value=max_length),
         )
 
-    def _make_personal_info_recipe(self):
+    def _make_group_common_recipe(
+        self, model_name: str, teachers_min_value: int, teachers_max_value: int
+    ) -> Recipe:
+        return Recipe(
+            model_name,
+            coordinators=lambda: related(
+                *[self.coordinator_recipe] * self.faker.pyint(min_value=1, max_value=5)
+            ),
+            students=lambda: related(
+                *[self.student_recipe] * self.faker.pyint(min_value=5, max_value=25)
+            ),
+            teachers=lambda: related(
+                *[self.teacher_recipe]
+                * self.faker.pyint(
+                    min_value=teachers_min_value, max_value=teachers_max_value
+                )
+            ),
+            telegram_chat_url=self.faker.url,
+        )
+
+    def _make_personal_info_recipe(self) -> Recipe:
         return Recipe(
             APP_NAME + ".PersonalInfo",
             # These are callables from faker to be called on recipe baking
@@ -91,7 +127,9 @@ class FakeDataPopulator(DataMigrationMaker):
             personal_info=foreign_key(self.personal_info_recipe, one_to_one=True),
             comment=self.faker.text,
             status=lambda: self.faker.random_element(StudentStatus.values),
-            age_range=lambda: self.faker.random_element(AgeRange.objects.filter(type=AgeRangeType.STUDENT)),
+            age_range=lambda: self.faker.random_element(
+                AgeRange.objects.filter(type=AgeRangeType.STUDENT)
+            ),
             availability_slots=lambda: self._get_random_amount_of_objects(
                 DayAndTimeSlot, min_length=10, max_length=20
             ),
@@ -141,7 +179,7 @@ class FakeDataPopulator(DataMigrationMaker):
 
     def _make_teacher_under_18_recipe(self):
         return Recipe(
-            # TODO: Think about recipe inheritance for Teacher, Person
+            # TODO: Think about recipe inheritance for Teacher, Person. See example with Group/SpeakingClub
             #  (https://model-bakery.readthedocs.io/en/latest/recipes.html#recipe-inheritance)
             APP_NAME + ".TeacherUnder18",
             personal_info=foreign_key(self.personal_info_recipe, one_to_one=True),
@@ -152,29 +190,86 @@ class FakeDataPopulator(DataMigrationMaker):
             is_validated=self.faker.pybool,
         )
 
-    def _make_fake_coordinators(self):
-        """Makes fake coordinators."""
-        self.coordinator_recipe.make(_quantity=AMOUNT_OF_COORDINATORS)
+    def __get_random_time_or_none(self):
+        return self.faker.random_element([None, self.faker.time_object()])
 
-    def _make_fake_students(self):
-        """Makes fake students."""
-        self.student_recipe.make(_quantity=AMOUNT_OF_STUDENTS)
+    def _make_group_recipe(self):
+        group_common_recipe = self._make_group_common_recipe(
+            APP_NAME + ".Group", teachers_min_value=1, teachers_max_value=3
+        )
+        return group_common_recipe.extend(
+            availability_slot=lambda: self._get_random_amount_of_objects(
+                DayAndTimeSlot, min_length=10, max_length=20
+            ),
+            is_for_staff_only=self.faker.pybool,
+            language_and_level=lambda: self.faker.random_element(
+                LanguageAndLevel.objects.all()
+            ),
+            lesson_duration_in_minutes=lambda: self.faker.pyint(
+                min_value=30, max_value=120, step=30
+            ),
+            status=lambda: self.faker.random_element(GroupStatus.values),
+            start_date=self.faker.past_date,
+            end_date=self.faker.future_date,
+            # FIXME: Sometimes all of the days are generated as None.
+            #  It violates the model's constraint.
+            #  Just restart the migration and pray to the God of Random.
+            monday=self.__get_random_time_or_none,
+            tuesday=self.__get_random_time_or_none,
+            wednesday=self.__get_random_time_or_none,
+            thursday=self.__get_random_time_or_none,
+            friday=self.__get_random_time_or_none,
+            saturday=self.__get_random_time_or_none,
+            sunday=self.__get_random_time_or_none,
+        )
 
-    def _make_fake_teachers(self):
-        """Makes fake teachers."""
-        self.teacher_recipe.make(_quantity=AMOUNT_OF_TEACHERS)
+    def _make_speaking_club_recipe(self):
+        group_common_recipe = self._make_group_common_recipe(
+            APP_NAME + ".SpeakingClub", teachers_min_value=0, teachers_max_value=2
+        )
+        return group_common_recipe.extend(
+            is_for_children=self.faker.pybool,
+            language=lambda: self.faker.random_element(Language.objects.all()),
+            teachers_under_18=lambda: related(
+                *[self.teacher_under_18_recipe]
+                * self.faker.pyint(min_value=0, max_value=2)
+            ),
+        )
 
-    def _make_fake_teachers_under_18(self):
-        """Makes fake teachers under 18."""
-        self.teacher_under_18_recipe.make(_quantity=AMOUNT_OF_TEACHERS_UNDER_18)
+    def _make_fake_coordinators_without_group(self):
+        """Makes fake coordinators without group."""
+        self.coordinator_recipe.make(_quantity=AMOUNT_OF_COORDINATORS_WITHOUT_GROUP)
+
+    def _make_fake_students_without_group(self):
+        """Makes fake students without group."""
+        self.student_recipe.make(_quantity=AMOUNT_OF_STUDENTS_WITHOUT_GROUP)
+
+    def _make_fake_teachers_without_group(self):
+        """Makes fake teachers without groups."""
+        self.teacher_recipe.make(_quantity=AMOUNT_OF_TEACHERS_WITHOUT_GROUP)
+
+    def _make_fake_teachers_under_18_without_speaking_club(self):
+        """Makes fake teachers under 18 without_speaking_club."""
+        self.teacher_under_18_recipe.make(
+            _quantity=AMOUNT_OF_TEACHERS_UNDER_18_WITHOUT_SPEAKING_CLUB
+        )
+
+    def _make_fake_groups(self):
+        """Makes fake groups."""
+        self.group_recipe.make(_quantity=AMOUNT_OF_GROUPS)
+
+    def _make_fake_speaking_clubs(self):
+        """Makes fake speaking_clubs."""
+        self.speaking_club_recipe.make(_quantity=AMOUNT_OF_SPEAKING_CLUBS)
 
     def _populate(self):
         """Runs pre-population operations."""
-        self._make_fake_students()
-        self._make_fake_coordinators()
-        self._make_fake_teachers()
-        self._make_fake_teachers_under_18()
-        # TODO Groups
+        self._make_fake_students_without_group()
+        self._make_fake_coordinators_without_group()
+        self._make_fake_teachers_without_group()
+        self._make_fake_teachers_under_18_without_speaking_club()
+        self._make_fake_groups()
+        self._make_fake_speaking_clubs()
 
 
 class Migration(migrations.Migration):
--- django_webapps/custom_baker.py
+++ django_webapps/custom_baker.py
@@ -1,5 +1,6 @@
 from django.db.models import Model
 from model_bakery.baker import Baker
+from model_bakery.recipe import related
 
 
 class CallableM2MBaker(Baker):
@@ -11,5 +12,10 @@ class CallableM2MBaker(Baker):
         """
         for key, values in self.m2m_dict.items():
             if callable(values):
-                self.m2m_dict[key] = values()
+                called_values = values()
+                if isinstance(called_values, related):
+                    # If the callable returns a related object, we need to use it as a recipe
+                    self.m2m_dict[key] = called_values.make()
+                else:
+                    self.m2m_dict[key] = called_values
         super()._handle_m2m(instance)
