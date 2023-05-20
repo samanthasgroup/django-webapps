660bf41e84cb3ec9c4455ff23d3ca848b1fc541a
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-05-20 16:10:55 +0300

    parametrize test to cover edge cases

--- tests/tests_api/test_enrollment_test.py
+++ tests/tests_api/test_enrollment_test.py
@@ -1,3 +1,4 @@
+import pytest
 from model_bakery import baker
 from rest_framework import status
 
@@ -32,21 +33,49 @@ def test_get_enrollment_test(api_client):
     ]
 
 
-def test_get_level(api_client):
+@pytest.mark.parametrize(
+    "total_answers, correct_answers, expected_level",
+    [
+        (35, 35, "C1"),
+        (35, 32, "C1"),
+        (35, 31, "B2"),
+        (35, 27, "B2"),
+        (35, 26, "B1"),
+        (35, 20, "B1"),
+        (35, 19, "A2"),
+        (35, 13, "A2"),
+        (35, 12, "A1"),
+        (35, 6, "A1"),
+        (35, 5, "A0"),
+        (35, 0, "A0"),
+        (25, 25, "B1"),
+        (25, 19, "B1"),
+        (25, 18, "A2"),
+        (25, 11, "A2"),
+        (25, 10, "A1"),
+        (25, 5, "A1"),
+        (25, 4, "A0"),
+        (25, 0, "A0"),
+    ],
+)
+def test_get_level(api_client, total_answers, correct_answers, expected_level):
     """Test calculating the resulting level of enrollment test."""
-    test = baker.make_recipe("tests.enrollment_test")
+    # Don't need to specify a test here, just take some correct and incorrect answers to any test
     correct_answers_ids = list(
-        EnrollmentTestQuestionOption.objects.filter(
-            question__enrollment_test=test, is_correct=True
-        ).values_list("id", flat=True)
-    )
+        EnrollmentTestQuestionOption.objects.filter(is_correct=True).values_list("id", flat=True)
+    )[:correct_answers]
+
+    incorrect_answers_ids = list(
+        EnrollmentTestQuestionOption.objects.filter(is_correct=False).values_list("id", flat=True)
+    )[: total_answers - correct_answers]
+
     response = api_client.post(
         "/api/enrollment_test_result/get_level/",
-        data={"answers": correct_answers_ids},
+        data={"answers": correct_answers_ids + incorrect_answers_ids},
     )
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
-        "resulting_level": "C1",
+        "resulting_level": expected_level,
     }
 
 
