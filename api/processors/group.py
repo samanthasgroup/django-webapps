fcf1acb4515292047fb8e5309d52a30769bbb327
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-06-11 21:31:15 +0300

    add filters to coordinators

--- api/processors/group.py
+++ api/processors/group.py
@@ -9,6 +9,7 @@ from api.models.choices.statuses import (
     StudentStatus,
     TeacherStatus,
 )
+from api.models.constants import CoordinatorGroupLimit
 from api.processors.base import Processor
 
 
@@ -19,8 +20,19 @@ class GroupProcessor(Processor):
 
         cls._set_status(obj=group, status=GroupStatus.WORKING, status_since=timestamp)
 
-        # FIXME set status depending on whether the threshold or limit is reached
-        group.coordinators.update(status=CoordinatorStatus.WORKING_OK, status_since=timestamp)
+        # cannot use "has_enough_groups" or "has_reached_group_limit" because they are properties
+        group.coordinators.filter(groups__count_lt=CoordinatorGroupLimit.MIN).update(
+            status=CoordinatorStatus.WORKING_BELOW_THRESHOLD, status_since=timestamp
+        )
+
+        group.coordinators.filter(
+            groups__count__gte=CoordinatorGroupLimit.MIN,
+            groups__count__lt=CoordinatorGroupLimit.MAX,
+        ).update(status=CoordinatorStatus.WORKING_OK, status_since=timestamp)
+
+        group.coordinators.filter(groups__count__gte=CoordinatorGroupLimit.MAX).update(
+            status=CoordinatorStatus.WORKING_LIMIT_REACHED, status_since=timestamp
+        )
 
         # FIXME set status depending on max_groups of teacher
         group.teachers.update(
