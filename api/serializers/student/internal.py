d6b644d156197ec11711a6d649213327bc38de7b
Roxe322 <kolomatskiy@bk.ru>
2023-06-11 18:02:37 +0400

    Students serializers placement refactoring

---
+++ api/serializers/student/internal.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from api.models import Student
+from api.serializers import (
+    AgeRangeSerializer,
+    DayAndTimeSlotSerializer,
+    LanguageAndLevelSerializer,
+)
+
+
+class StudentWriteSerializer(serializers.ModelSerializer[Student]):
+    class Meta:
+        model = Student
+        exclude = ("children",)
+
+
+class StudentReadSerializer(serializers.ModelSerializer[Student]):
+    age_range = AgeRangeSerializer(read_only=True)
+    teaching_languages_and_levels = LanguageAndLevelSerializer(many=True, read_only=True)
+    availability_slots = DayAndTimeSlotSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Student
+        exclude = ("children",)
