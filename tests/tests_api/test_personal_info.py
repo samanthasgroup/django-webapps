7f447764d539799a45c62bb7bfaa3ac01e190aa0
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-07-16 14:59:10 +0300

    move and rename tests with no changes

--- tests/tests_api/test_personal_info.py
+++ tests/tests_api/test_personal_info.py
@@ -28,6 +28,39 @@ def test_personal_info_create(api_client, fake_personal_info_data):
     assert PersonalInfo.objects.filter(**fake_personal_info_data).exists()
 
 
+def test_personal_info_get_with_params_returns_200_with_existing_chat_id(
+    api_client, fake_personal_info_data
+):
+    api_client.post("/api/personal_info/", data=fake_personal_info_data)
+
+    response = api_client.get(
+        path="/api/personal_info/",
+        params={
+            "registration_telegram_bot_chat_id": fake_personal_info_data[
+                "registration_telegram_bot_chat_id"
+            ]
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+    # checking some basic attrs (cannot compare two objects directly
+    # because some fields were added during creation)
+    for param in ["first_name", "last_name", "email", "information_source"]:
+        assert response.json()[0][param] == fake_personal_info_data[param]
+
+
+def test_personal_info_get_with_params_returns_406_with_unknown_chat_id(
+    api_client, fake_personal_info_data
+):
+    api_client.post("/api/personal_info/", data=fake_personal_info_data)
+    non_existent_chat_id = fake_personal_info_data["registration_telegram_bot_chat_id"] + 1
+    response = api_client.get(
+        path=f"/api/personal_info/?registration_telegram_bot_chat_id={non_existent_chat_id}",
+    )
+    assert response.status_code == status.HTTP_406_NOT_ACCEPTABLE
+    assert response.json() == {"detail": "No object with this data exists."}
+
+
 def test_personal_info_update(api_client, fake_personal_info_data):
     # seq is used here to resolve compound unique constraint
     personal_info = baker.make(PersonalInfo, first_name=seq("Ivan"))
@@ -117,36 +150,3 @@ def test_personal_info_check_existence_of_chat_id_returns_400_with_no_id(
     response = api_client.get(path="/api/personal_info/check_existence_of_chat_id/")
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {"registration_telegram_bot_chat_id": ["This field is required."]}
-
-
-def test_personal_info_with_params_returns_200_with_existing_id(
-    api_client, fake_personal_info_data
-):
-    api_client.post("/api/personal_info/", data=fake_personal_info_data)
-
-    response = api_client.get(
-        path="/api/personal_info/",
-        params={
-            "registration_telegram_bot_chat_id": fake_personal_info_data[
-                "registration_telegram_bot_chat_id"
-            ]
-        },
-    )
-    assert response.status_code == status.HTTP_200_OK
-
-    # checking some basic attrs (cannot compare two objects directly
-    # because some fields were added during creation)
-    for param in ["first_name", "last_name", "email", "information_source"]:
-        assert response.json()[0][param] == fake_personal_info_data[param]
-
-
-def test_personal_info_with_params_returns_406_with_unknown_id(
-    api_client, fake_personal_info_data
-):
-    api_client.post("/api/personal_info/", data=fake_personal_info_data)
-    non_existent_chat_id = fake_personal_info_data["registration_telegram_bot_chat_id"] + 1
-    response = api_client.get(
-        path=f"/api/personal_info/?registration_telegram_bot_chat_id={non_existent_chat_id}",
-    )
-    assert response.status_code == status.HTTP_406_NOT_ACCEPTABLE
-    assert response.json() == {"detail": "No object with this data exists."}
