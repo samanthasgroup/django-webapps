379a90af6505c41cb2db102772fb3e6d51218c05
Polina Matavina <paulin.mipt@gmail.com>
2023-08-06 16:00:58 +0100

    fix noqa

--- scripts/run_group_creation.py
+++ scripts/run_group_creation.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_webapps.settings")
 django.setup()
 
-from api.group_creation.group_manager import GroupManager  # noqa(E402)
+from api.group_creation.group_manager import GroupManager  # noqa: E402
 
 if __name__ == "__main__":
     for teacher in GroupManager.get_available_teachers():
