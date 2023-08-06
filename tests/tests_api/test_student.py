66fef0e6148ab11066f4c5e6d7378fe09ac07d32
auch-hunter <auchhunter@gmail.com>
2023-08-06 22:18:48 +0200

    fix(tests API): revert student retrieve asserts

--- tests/tests_api/test_student.py
+++ tests/tests_api/test_student.py
@@ -13,11 +13,7 @@ from api.models import (
     Student,
 )
 from api.models.choices.status import StudentStatus
-from api.serializers import (
-    DashboardStudentSerializer,
-    StudentReadSerializer,
-    StudentWriteSerializer,
-)
+from api.serializers import DashboardStudentSerializer, StudentWriteSerializer
 from tests.tests_api.asserts import assert_response_data, assert_response_data_list
 
 
@@ -70,8 +66,50 @@ def test_student_retrieve(api_client, availability_slots):
     student = baker.make(Student, make_m2m=True, availability_slots=availability_slots)
     response = api_client.get(f"/api/students/{student.personal_info.id}/")
 
+    response_json = response.json()
     assert response.status_code == status.HTTP_200_OK
-    assert_response_data(response.data, StudentReadSerializer(student).data)
+    languages_and_levels = [
+        {
+            "id": language_and_level.id,
+            "language": {
+                "id": language_and_level.language.id,
+                "name": language_and_level.language.name,
+            },
+            "level": language_and_level.level.id,
+        }
+        for language_and_level in student.teaching_languages_and_levels.all()
+    ]
+    availability_slots = [
+        {
+            "id": slot.id,
+            "time_slot": {
+                "id": slot.time_slot.id,
+                "from_utc_hour": str(slot.time_slot.from_utc_hour),
+                "to_utc_hour": str(slot.time_slot.to_utc_hour),
+            },
+            "day_of_week_index": slot.day_of_week_index,
+        }
+        for slot in student.availability_slots.all()
+    ]
+    assert response_json == {
+        "personal_info": student.personal_info.id,
+        "age_range": {
+            "id": student.age_range.id,
+            "age_from": student.age_range.age_from,
+            "age_to": student.age_range.age_to,
+            "type": student.age_range.type,
+        },
+        "teaching_languages_and_levels": languages_and_levels,
+        "availability_slots": availability_slots,
+        "comment": student.comment,
+        "status": student.status,
+        "status_since": student.status_since.isoformat().replace("+00:00", "Z"),
+        "is_member_of_speaking_club": student.is_member_of_speaking_club,
+        "can_read_in_english": student.can_read_in_english,
+        # These are optional, so baker won't generate them (unless _fill_optional is True)
+        "non_teaching_help_required": [],
+        "smalltalk_test_result": None,
+    }
 
 
 def test_student_update(api_client, availability_slots):
