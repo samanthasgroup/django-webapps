c546552b0509cf2069d8e49fb6f95386c2046d1c
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2024-05-23 14:33:32 +0300

    leave only `id` and `full_name` clickable

--- api/admin/coordinator.py
+++ api/admin/coordinator.py
@@ -157,14 +157,6 @@ class CoordinatorAdmin(VersionAdmin):
     list_display_links = (
         "get_personal_info_id",
         "get_personal_info_full_name",
-        "get_additional_skills_comment",
-        "get_project_status",
-        "get_situational_status",
-        "get_status_since",
-        "active_groups_count",
-        "get_communication_language_mode",
-        "mentor",
-        "get_comment",
     )
 
     class Media:
